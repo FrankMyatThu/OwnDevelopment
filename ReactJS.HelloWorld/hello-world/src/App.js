bf6e6589b43d3cf0c6fca76245b0133463a66d1a
Myat Thu <myatthu1986@gmail.com>
2020-02-02 18:51:36 +0800

    changed default export to name export

--- ReactJS.HelloWorld/hello-world/src/App.js
+++ ReactJS.HelloWorld/hello-world/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-import Greet from './components/Greet'
+import { Greet } from './components/Greet'
 
 class App extends Component {
   render() {
--- ReactJS.HelloWorld/hello-world/src/components/Greet.js
+++ ReactJS.HelloWorld/hello-world/src/components/Greet.js
@@ -1,5 +1,3 @@
 import React from 'react'
 
-const Greet = () => <h1>Hello Frank via Greet functional component using ES6 syntax</h1>
-
-export default Greet
+export const Greet = () => <h1>Hello Frank via Greet functional component using ES6 syntax, name export method </h1>
