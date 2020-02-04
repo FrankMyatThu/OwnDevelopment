bf40d5ec111c7ef53abe44d22c4ac25a9b03a86e
Myat Thu <myatthu1986@gmail.com>
2020-02-04 21:43:30 +0800

    JSX Hello

--- ReactJS.HelloWorld/hello-world/src/App.js
+++ ReactJS.HelloWorld/hello-world/src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import { Component } from 'react';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+import Hello from './components/Hello'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet></Greet>
-        <Welcome></Welcome>
+        {/* <Greet></Greet>        
+        <Welcome></Welcome> */}
+        <Hello></Hello>
       </div>
     );
   }
---
+++ ReactJS.HelloWorld/hello-world/src/components/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hello = () =>{
+    return(
+        <div className="test">
+            <h1>Hello Frank from HELLO JSX</h1>
+        </div>
+    )
+}
+
+export default Hello
