8621cfc6e4d73f735202a91c136c28aed14892b4
Myat Thu <myatthu1986@gmail.com>
2020-02-04 21:55:50 +0800

    React.creatElement

--- ReactJS.HelloWorld/hello-world/src/components/Hello.js
+++ ReactJS.HelloWorld/hello-world/src/components/Hello.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const Hello = () =>{
-    return(
-        <div className="test">
-            <h1>Hello Frank from HELLO JSX</h1>
-        </div>
-    )
+    // return(
+    //     <div className="dummyClass">
+    //         <h1>Hello Frank from HELLO JSX</h1>
+    //     </div>
+    // )
+
+    return React.createElement(
+        'div', 
+        {id: 'hello', className: 'dummyClass'}, 
+        React.createElement('h1', null, 'Hello Frank from HELLO React.creatElement'))
+
 }
 
 export default Hello
