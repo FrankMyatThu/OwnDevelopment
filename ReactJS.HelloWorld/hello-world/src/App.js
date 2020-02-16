b8980b18d37b7f7d6265345636e4574499aa46e6
Myat Thu <myatthu1986@gmail.com>
2020-02-16 18:43:57 +0800

    type react snippet " rce " to call es7 code snippets

--- ReactJS.HelloWorld/hello-world/src/App.js
+++ ReactJS.HelloWorld/hello-world/src/App.js
@@ -6,12 +6,14 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
+import Counter from './components/Counter'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Message></Message>
+        <Counter></Counter>
+        {/* <Message></Message> */}
         {/* <Greet name="Frank" customerName="Batman">
           <p>This is a paragraph</p>
         </Greet> 
