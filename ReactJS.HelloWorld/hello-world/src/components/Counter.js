8746c1b22d77e2e6a3020d12c40494ec7a6637c5
Myat Thu <myatthu1986@gmail.com>
2020-02-16 19:21:51 +0800

    setState and async callback

--- ReactJS.HelloWorld/hello-world/src/components/Counter.js
+++ ReactJS.HelloWorld/hello-world/src/components/Counter.js
@@ -1,10 +1,40 @@
+// rce
 import React, { Component } from 'react'
 
 class Counter extends Component {
+
+    // rconst 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            count: 0
+        }
+    }
+
+    increment() {
+        // without using setState, it cannot auto-update to UI value
+        // this.state.count = this.state.count + 1
+
+        this.setState({
+            count: this.state.count + 1
+        }, () => { console.log('Callback value', this.state.count) })
+        console.log(this.state.count)
+    }
+
+    incrementFive() {
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+    }
+
     render() {
         return (
             <div>
-                count
+                <div>Count - {this.state.count}</div>
+                <button onClick={() => this.increment()} >Increment</button>
             </div>
         )
     }
