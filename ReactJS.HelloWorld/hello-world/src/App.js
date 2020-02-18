faddd196050eda56daedb5405dceb18414370189
Myat Thu <myatthu1986@gmail.com>
2020-02-18 20:56:28 +0800

    this keyword using bind

--- ReactJS.HelloWorld/hello-world/src/App.js
+++ ReactJS.HelloWorld/hello-world/src/App.js
@@ -9,13 +9,15 @@ import Message from './components/Message'
 import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
+import EventBind from './components/EventBind';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FunctionClick></FunctionClick>
-        <ClassClick></ClassClick>
+        <EventBind></EventBind>
+        {/* <FunctionClick></FunctionClick>
+        <ClassClick></ClassClick> */}
         {/* <Welcome name="Frank" customerName="Batman"></Welcome> */}
         {/* <Greet name="FrankMyatThu" customerName="Superman"></Greet> */}
         {/* <Counter></Counter> */}
