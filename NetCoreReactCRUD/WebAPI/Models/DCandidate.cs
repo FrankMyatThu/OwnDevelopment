6834834a2446b2b000056f2e9b64ca4848572e72
Frank Myat Thu <frank.myatthu@brc.com.sg>
2020-04-08 20:22:46 +0800

    NetCoreReactCRUD

---
+++ NetCoreReactCRUD/WebAPI/Models/DCandidate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+	public class DCandidate
+	{
+		[Key]
+		public int id { get; set; }
+
+		[Column(TypeName = "nvarchar(100)")]
+		public string fullName { get; set; }
+
+		[Column(TypeName = "nvarchar")]
+		public string mobile { get; set; }
+
+		[Column(TypeName = "nvarchar")]
+		public string email { get; set; }
+				
+		public int age { get; set; }
+
+		[Column(TypeName = "nvarchar")]
+		public int bloodGroup { get; set; }
+
+		[Column(TypeName = "nvarchar")]
+		public string address { get; set; }
+	}
+}
