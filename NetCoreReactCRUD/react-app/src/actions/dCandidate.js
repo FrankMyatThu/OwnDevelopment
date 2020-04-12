e63ecae405dbd24d44d439651b2560444597a3e0
Frank Myat Thu <frank.myatthu@brc.com.sg>
2020-04-12 14:01:51 +0800

    start action componets reducers

---
+++ NetCoreReactCRUD/react-app/src/actions/dCandidate.js
@@ -0,0 +1,13 @@
+export const ACTION_TYPES = {
+    CREATE:'CREATE',
+    UPDATE:'UPDATE',
+    DELETE:'DELETE',
+    FETCH_ALL:'FETCH_ALL'
+}
+
+export const fetchall = () => dispatch => {
+    dispatch({
+        type : ACTION_TYPES.FETCH_ALL,
+        payload : []
+    })
+}
