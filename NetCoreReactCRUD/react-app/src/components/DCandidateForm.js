138569350f4dce63eba06459fd9948a6bb785668
Frank Myat Thu <frank.myatthu@brc.com.sg>
2020-04-27 20:08:19 +0800

    added select dropdown

--- NetCoreReactCRUD/react-app/src/components/DCandidateForm.js
+++ NetCoreReactCRUD/react-app/src/components/DCandidateForm.js
@@ -1,5 +1,19 @@
 import React, {useState} from "react";
-import { Grid, TextField } from "@material-ui/core";
+import { Grid, TextField, withStyles, MenuItem, FormControl, InputLabel, Select } from "@material-ui/core";
+import useForm from "./useForm";
+
+const styles = theme => ({
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            minWidth: 230,
+          }
+    },
+    formControl:{
+        margin: theme.spacing(1),
+        minWidth: 230,
+    }
+})
 
 const initialFieldValues = {
     FullName: '',
@@ -10,17 +24,22 @@ const initialFieldValues = {
     address: ''
 }
 
-const DCandidateForm = (props) => {
-    const [values, setValues] = useState(initialFieldValues)
-    const handleInputChange = e => {
-        const [name, value] = e.target
-        setValues({
-            ...values,
-            [name]: value
-        })
-    }
+const DCandidateForm = ({classes, ...props}) => {    
+    const {
+        values,
+        setValues,
+        handleInputChange
+    } = useForm(initialFieldValues)
+
+    // material-ui select
+    const inputLabel = React.useRef(null);
+    const [labelWidth, setLabelWidth] = React.useState(0);
+    React.useEffect(() => {
+        setLabelWidth(inputLabel.current.offsetWidth);
+    }, []);
+
     return(
-        <form autoComplete="off" noValidate>
+        <form autoComplete="off" noValidate className={classes.root}>
             <Grid container >
                 <Grid item xs = {6}>
                     <TextField 
@@ -37,7 +56,24 @@ const DCandidateForm = (props) => {
                         value={values.email}
                         onChange= {handleInputChange} 
                     />
-                    <div>bloodGroup</div>
+                    <FormControl variant="outlined" className={classes.formControl}>
+                        <InputLabel ref={inputLabel}>Blood Group</InputLabel>
+                        <Select 
+                            name="bloodGroup"
+                            value={values.bloodGroup}
+                            onChange={handleInputChange}
+                            labelWidth={labelWidth} >
+                            <MenuItem value="" >Select Blood Group</MenuItem>
+                            <MenuItem value="A+">A +ve</MenuItem>
+                            <MenuItem value="A-">A -ve</MenuItem>
+                            <MenuItem value="B+">B +ve</MenuItem>
+                            <MenuItem value="B-">B -ve</MenuItem>
+                            <MenuItem value="AB+">AB +ve</MenuItem>
+                            <MenuItem value="AB-">AB -ve</MenuItem>
+                            <MenuItem value="O+">O +ve</MenuItem>
+                            <MenuItem value="O-">O -ve</MenuItem>
+                        </Select>
+                    </FormControl>
                 </Grid>
                 <Grid item xs={6}>
                     <TextField 
@@ -67,4 +103,4 @@ const DCandidateForm = (props) => {
     );
 }
 
-export default DCandidateForm;
+export default withStyles(styles) (DCandidateForm);
